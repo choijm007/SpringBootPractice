a6db53914848fa3a7fd0679ee25daaff4cb410b4
choijaemin <choijaemin16@naver.com>
2024-08-07 21:08:16 +0900

    SpringBoot 기초 완료

---
+++ src/main/java/me/choijaemin/springbootdeveloper/Member.java
@@ -0,0 +1,23 @@
+package me.choijaemin.springbootdeveloper;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+
+}
